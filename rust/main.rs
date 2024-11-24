e06b2b5f7383bc96e370c4200caf91e6f072875f
hermann wenninger <info@wennhmann.de>
2024-11-24 12:28:56 +0100

    owner3

--- rust/main.rs
+++ rust/main.rs
@@ -1,32 +1,9 @@
 fn main(){
     
-    let x = calc_string_len("hello".to_string());
-    println!("{}{}", x.0, x.1);
-    let a = "hello1".to_string();
-    println!("{}", a);
-    {
-        let a = "hello2";
-        println!("{}", a);
-        
-    }
-    println!("{}", a);
+ 
 
-    fn calc_string_len(s:String) ->(String, usize){
-        let l = s.len();
-        (s, l)
+
+
+
+
     }
-    let y = calc_ref_len("hellooooo");
-    println!("hello{}", y);
-    fn calc_ref_len(s:&str) ->usize{
-         s.len()
-      }
-      let mut s = "hello".to_string();
-      fn change_string(s: &mut String){
-          s.push_str("101");
-      }
-        change_string(&mut s);
-        println!("{}", s);
-        let word: String = String::from("abra kadabra pro labra");
-        let a_word = &word[0..5];
-        println!("{}", a_word);
-}
