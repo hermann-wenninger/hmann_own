94483bd0f5a6715651444dcb2787d7df41fa370f
hermann wenninger <info@wennhmann.de>
2024-11-24 17:08:13 +0100

    threadnings1

--- rust/main.rs
+++ rust/main.rs
@@ -2,21 +2,21 @@ fn main(){
     use std::thread;
 
         println!("Hello from the main thread ....... 1.");
-        thread::spawn(fd);
+       let t1 = thread::spawn(fd);
         println!("Hello from the main thread ........2.");
-        thread::spawn(fd);
+      let t2 = thread::spawn(fd);
         println!("Hello from the main thread ........3.");
-        thread::spawn(fd);
+      let t3 =  thread::spawn(fd);
         println!("Hello from the main thread ........4.");
-        thread::spawn(fd);
+      let t4 =  thread::spawn(fd);
         println!("Hello from the main thread ........5.");
-        thread::spawn(fd);
+      let t5 =  thread::spawn(fd);
         println!("Hello from the main thread ........6.");
-        thread::spawn(fd);
+      let t6 =  thread::spawn(fd);
         println!("Hello from the main thread ........7.");
-        thread::spawn(fd);
+       let t7 = thread::spawn(fd);
         println!("Hello from the main thread ........8.");
-        thread::spawn(fd);
+       let t8 = thread::spawn(fd);
         
     
         println!("Hello from the main thread ........end.");
@@ -28,5 +28,12 @@ fn main(){
         let id = thread::current().id();
         println!("This is my thread id: {id:?}");
     }
-
+    t1.join().unwrap();
+    t2.join().unwrap();
+    t3.join().unwrap();
+    t4.join().unwrap();
+    t5.join().unwrap();
+    t6.join().unwrap();
+    t7.join().unwrap();
+    t8.join().unwrap();
     }
