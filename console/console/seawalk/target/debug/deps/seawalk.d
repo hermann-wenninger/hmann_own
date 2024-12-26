3434784b861be8dff5d0a60e144abf62179027e7
hermann wenninger <info@wennhmann.de>
2024-12-26 15:00:07 +0100

    seawalk

---
+++ console/console/seawalk/target/debug/deps/seawalk.d
@@ -0,0 +1,5 @@
+D:\a_work\hmann_own\console\console\seawalk\target\debug\deps\seawalk.exe: src/main.rs
+
+D:\a_work\hmann_own\console\console\seawalk\target\debug\deps\seawalk.d: src/main.rs
+
+src/main.rs:
---
+++ console/console/seawalk/target/debug/seawalk.d
@@ -0,0 +1,1 @@
+D:\a_work\hmann_own\console\console\seawalk\target\debug\seawalk.exe: D:\a_work\hmann_own\console\console\seawalk\src\main.rs
